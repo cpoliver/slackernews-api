ff3c73084a8668dc639802f94245f25c63c19aa6
Charles Oliver <charles.oliver@casumo.com>
2018-07-19 13:56:22 +0100

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-    extends: ['casumo/configurations/es6-node', 'casumo/configurations/es6-test', 'casumo/configurations/es6-browser'],
+    extends: [
+        'casumo/configurations/es6-node',
+        'casumo/configurations/es6-test',
+        'casumo/configurations/es6-browser',
+        'prettier',
+    ],
     plugins: ['prettier'],
     rules: {
         'prettier/prettier': 'error',
-        'comma-dangle': 'off',
     },
 };
