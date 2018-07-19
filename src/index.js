70c9c214c5ce41e6a767b42c1ba4776bac3568b2
Charles Oliver <charles.oliver@casumo.com>
2018-07-19 14:06:06 +0100

    add basic link type/resolver

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
     type Query {
       info: String!
+      feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!,
+        description: String!,
+        url: String!,
     }
 `;
+
+const links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Full-stack tutorial fro GraphQL',
+    },
+];
 
 const resolvers = {
     Query: {
         info: () => 'This is the Slackernews API',
+        feed: () => links,
+    },
+    Link: {
+        id: root => root.id.toUpperCase(),
+        description: root => root.description,
+        url: root => root.url,
     },
 };
 
